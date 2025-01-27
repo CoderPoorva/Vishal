8b6cd8e9f301383760de3b25dc3e903b233f0eb2
Poorva Adhikary <adhikarypoorvaraja@gmail.com>
2025-01-27 14:53:04 +0530

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-import PlayCircleOutlineRoundedIcon from '@mui/icons-material/PlayCircleOutlineRounded';
+import PlayCircleOutlineRoundedIcon from "@mui/icons-material/PlayCircleOutlineRounded";
 import Confetti from "react-confetti";
 import Envelope from "../public/image/envelope.png";
 import Couple from "../public/image/Couple.jpg";
@@ -11,11 +11,9 @@ const App = () => {
   const [showButton, setShowButton] = useState(false);
   const [showConfetti, setShowConfetti] = useState(false);
 
-
   const nextStep = () => setStep(step + 1);
 
   const [audio] = useState(new Audio("/src/assets/Audio/bgmusic.mp3"));
-  
 
   const startAudio = () => {
     audio.play();
@@ -25,20 +23,19 @@ const App = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setShowButton(true);
-    }, 10000); 
+    }, 10000);
 
-    return () => clearTimeout(timer); 
+    return () => clearTimeout(timer);
   }, []);
 
   const handleClick = () => {
     setStep(step + 1);
     setShowConfetti(true);
 
-
     // Stop confetti after 5 seconds
     setTimeout(() => {
       setShowConfetti(false);
-    }, 5000); // 
+    }, 5000); //
   };
 
   return (
@@ -49,7 +46,11 @@ const App = () => {
           <p style={{ fontSize: "25px", color: "white" }}>
             Your journey begins here...
           </p>
-          <button className="next-button" onClick={nextStep} style={{marginTop:"20px"}}>
+          <button
+            className="next-button"
+            onClick={nextStep}
+            style={{ marginTop: "20px" }}
+          >
             Start
           </button>
         </div>
@@ -58,15 +59,23 @@ const App = () => {
       {step === 0 && (
         <div className="welcome-screen">
           <h1>Let's Play the Music</h1>
-          <div className="audio-player" style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
-          <button className="Play-btn" onClick={startAudio}>
-            <PlayCircleOutlineRoundedIcon style={{fontSize:"2rem"}}/>
-          </button>
-          {showButton && <button className="next-button" onClick={nextStep}>
-           Next
-          </button>}
+          <div
+            className="audio-player"
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <button className="Play-btn" onClick={startAudio}>
+              <PlayCircleOutlineRoundedIcon style={{ fontSize: "2rem" }} />
+            </button>
+            {showButton && (
+              <button className="next-button" onClick={nextStep}>
+                Next
+              </button>
+            )}
           </div>
-          
         </div>
       )}
 
@@ -85,9 +94,9 @@ const App = () => {
       {step === 2 && (
         <div className="question-screen">
           <h2>
-            It has been almost five years since we became friends 🤝. It has taken
-            me two years to realize I have feelings for you 🥰. Do you have that
-            one feeling for me  👉👈?
+            It has been almost five years since we became friends 🤝. It has
+            taken me two years to realize I have feelings for you 🥰. Do you
+            have that one feeling for me  👉👈?
           </h2>
           <button className="next-button" onClick={nextStep}>
             Next
@@ -98,11 +107,7 @@ const App = () => {
       {step === 3 && (
         <div className="envelope-screen">
           <div className="envelope" onClick={nextStep}>
-            <img
-              src={Envelope}
-              alt=""
-              style={{ width: "100px" }}
-            />
+            <img src={Envelope} alt="" style={{ width: "100px" }} />
           </div>
           <p style={{ fontSize: "25px", color: "white" }}>
             Click the envelope to open a special letter...
@@ -115,26 +120,25 @@ const App = () => {
           <div className="letter">
             <p>Dear Love,</p>
             <p>
-              I want to let you know that I like you more than a
-              friend. We have been good friends, but you are more than a friend
-              to me. I like you and want our relationship to be more than
-              friendship. I have decided not to be shy about saying what I have
-              been feeling for so long. I love you....🌻❤️, and I have been
-              hiding it since we met. My feelings for you are not worth keeping
-              inside. They need to be showcased in front of you. Let me do
-              it...... 🥺 I love you Sampurna AKA Golu ( My one and only love )
-              😘. Do you love me......? If your answer is yes,then visit to the next page.
-              I have dedicated a poem for you written by me. If you like it, I also
-              deserve a kiss 🥺 .
+              I want to let you know that I like you more than a friend. We have
+              been good friends, but you are more than a friend to me. I like
+              you and want our relationship to be more than friendship. I have
+              decided not to be shy about saying what I have been feeling for so
+              long. I love you....🌻❤️, and I have been hiding it since we met.
+              My feelings for you are not worth keeping inside. They need to be
+              showcased in front of you. Let me do it...... 🥺 I love you
+              Sampurna AKA Golu ( My one and only love ) 😘. Do you love
+              me......? If your answer is yes,then visit to the next page. I
+              have dedicated a poem for you written by me. If you like it, I
+              also deserve a kiss 🥺 .
             </p>
             <p>Will you be mine forever?</p>
             <p>With all my love,</p>
             <p>Vishal</p>
           </div>
-          <button  onClick={handleClick}  className="next-button" >
+          <button onClick={handleClick} className="next-button">
             Next
           </button>
-          
         </div>
       )}
 
@@ -143,25 +147,34 @@ const App = () => {
           {showConfetti && <Confetti />}
           <p>
             তোমার আঙুলে রেখেছি আঙুল ছুঁয়ে <br />
-            শহরের ভিড়ে দেখেছি তোমার চোখ ।<br /><br />
+            শহরের ভিড়ে দেখেছি তোমার চোখ ।<br />
+            <br />
             বিকল যত অন্ধ চিন্তা আসে <br />
-            চাইছি তারা আজকে শুন্য হোক ।<br /><br />
+            চাইছি তারা আজকে শুন্য হোক ।<br />
+            <br />
             উড়ে যাক সব হিসেবের কালোচিঠি <br />
-            এমনিও তুমি বন্যা হয়েই আসো ।<br /><br />
+            এমনিও তুমি বন্যা হয়েই আসো ।<br />
+            <br />
             না বলা কথা জড়িয়ে যায় মুখে <br />
-            বোঝা যায় তুমি আমাকেই ভালোবাসো ।<br /><br />
+            বোঝা যায় তুমি আমাকেই ভালোবাসো ।<br />
+            <br />
             আসমানী যত পুরোনো তারার ভিড়ে <br />
-            সাজিয়ে রাখবো তোমায় নিজের ঘরে ।<br /><br />
+            সাজিয়ে রাখবো তোমায় নিজের ঘরে ।<br />
+            <br />
             তুমিও পারলে একখানা ছবি রেখো
             <br />
-            যাতে যখন তখন আমায় মনে পরে ।<br /><br />
+            যাতে যখন তখন আমায় মনে পরে ।<br />
+            <br />
             চলো আবার দুজন সাদামাটা প্রেম করি
             <br />
-            বেশিকিছু আমি চাইনা তোমার থেকে ।<br /><br />
+            বেশিকিছু আমি চাইনা তোমার থেকে ।<br />
+            <br />
             তোমার ভীষণ ব্যস্ত সময় ঘড়ির জালে <br />
-            আমাকে তুমি এক কোণে দিও রেখে ।<br /><br />
+            আমাকে তুমি এক কোণে দিও রেখে ।<br />
+            <br />
             রাখবনা আমি মাঝখানে আর কিছু <br />
-            মন মানেনা আমার তোমাকে ছাড়া ।<br /><br />
+            মন মানেনা আমার তোমাকে ছাড়া ।<br />
+            <br />
             আসলে তোমায় আমি চাইছি এত বেশি <br />
             তোমাকে ছাড়া আমি দিশেহারা ।
             <br />
@@ -173,16 +186,32 @@ const App = () => {
       )}
       {step === 6 && (
         <div className="end-screen">
-          <div className="image-container" style={{display:"flex",justifyContent:"center",alignItems:"center", marginBottom: "20px"}}>
-        <img
-          src={Couple}
-          alt="Couple"
-          className="couple-image"
-        />
-      </div>
-      <h1 className="love-text">I LOVE YOU ❤️</h1>
+          <div
+            className="image-container"
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              marginBottom: "20px",
+            }}
+          >
+            <img src={Couple} alt="Couple" className="couple-image" />
+          </div>
+          <h1 className="love-text">I LOVE YOU ❤️</h1>
         </div>
       )}
+      <footer  style={{
+        backgroundColor: "#333",
+        color: "#fff",
+        textAlign: "center",
+        padding: "5px 0",
+        fontSize: "14px",
+        position: "fixed",
+        bottom: 0,
+        width: "100%",
+      }}>
+        <p>Made with ❤️ by Poorva</p>
+      </footer>
     </div>
   );
 };
